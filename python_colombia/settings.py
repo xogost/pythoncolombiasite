44d291c3e33585d5665034c6b7e46459beac48d0
xogost <xogost@gmail.com>
2017-05-20 13:25:20 -0500

    Se permiten los host *

--- python_colombia/settings.py
+++ python_colombia/settings.py
@@ -91,6 +91,9 @@ DATABASES = {
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
